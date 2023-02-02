b662caa1b9ca2a3faf1ae3c3b67e190832cf0778
Alexandre Roux <alex@tekartik.com>
2023-02-02 12:54:09 +0100

    feat: simple app

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# analysis_server_crash.dart
-Demo of a simple dart analyser crash using dart 2.19..1
+Demoing a dart server analyser test.
---
+++ test/analysis_server_crash_test.dart
@@ -0,0 +1,13 @@
+import 'package:cv/cv.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('dummy', () {});
+}
+
+abstract class MyModel implements CvModel {}
+
+mixin MyModelMixin implements MyModel {}
+
+// This makes dart analyzer crashes, if you comment it, it is ok
+abstract class Test extends CvModelBase with MyModelMixin implements MyModel {}
